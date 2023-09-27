4a6f9c0063ac6c74bf1e4e9aa45b92e743e54758
김종식 <kim.jongsik@lumir-inc.com>
2023-09-27 11:13:28 +0900

    validate 라이브러리 설치

--- src/user/dto/req.dto.ts
+++ src/user/dto/req.dto.ts
@@ -1,7 +1,23 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class SignUpReqDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsOptional()
+  @IsString()
   company?: string;
+
+  @IsOptional()
+  @IsString()
   role?: string;
 }
