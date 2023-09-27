60f13ed5540884dc9027ae84eaa659bf1572e187
김종식 <kim.jongsik@lumir-inc.com>
2023-09-27 11:09:23 +0900

    회원가입 검증 http 작성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,7 @@ import { User } from 'src/schema/user/user';
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
-  @Post()
+  @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() signUpReqDto: SignUpReqDto): Promise<User> {
     return this.UserService.create(signUpReqDto);
