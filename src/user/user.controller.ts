c3f9995f41146a56458fc7f5c438643d5a89ec29
KJS <otw1917@naver.com>
2023-09-27 16:53:27 +0900

    유저회원가입 암호 해싱작업 수행

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,14 @@ import { UserService } from './user.service';
 import { Controller, HttpStatus, Post, HttpCode, Body } from '@nestjs/common';
 import { SignUpReqDto } from './dto/req.dto';
 import { User } from 'src/schema/user/user';
+import { Public } from 'src/decorators/public-api.decoratpr';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
   @Post('signup')
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() signUpReqDto: SignUpReqDto): Promise<User> {
     return this.UserService.create(signUpReqDto);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/schema/user/user';
 import { SignUpReqDto } from './dto/req.dto';
+import * as bcrypt from 'bcrypt';
+import { ExceptionMassage } from 'src/enums/excepion/exception';
 
 @Injectable()
 export class UserService {
@@ -10,27 +12,63 @@ export class UserService {
     @InjectModel(User.name) private userRepository: Model<UserDocument>,
   ) {}
 
+  /**
+   * Create a new user.
+   * @param signUpReqDto - The user information.
+   * @returns A promise that resolves to the created User object.
+   */
   async create(signUpReqDto: SignUpReqDto): Promise<User> {
+    await this.validateNewUser(signUpReqDto);
+
+    const hashedPassword = await this.hashPassword(signUpReqDto.password);
+
+    const createdUser = {
+      ...signUpReqDto,
+      password: hashedPassword,
+    };
+
+    return this.userRepository.create(createdUser);
+  }
+
+  /**
+   * Validate new user's information.
+   * @param signUpReqDto - The user information.
+   * @returns A promise that resolves when the validation is done.
+   */
+  async validateNewUser(signUpReqDto: SignUpReqDto): Promise<void> {
     const user = await this.findOneByEmail(signUpReqDto.email);
 
     if (user) {
       throw new BadRequestException({
-        message: 'Email already exists',
-        at: 'UserService.create',
+        message: ExceptionMassage.USER_ALREADY_EXISTS,
+        at: 'UserService.validateNewUser',
       });
     }
 
     if (signUpReqDto.password !== signUpReqDto.passwordConfirmation) {
-      throw new BadRequestException(
-        'Password and password confirmation must be the same',
-      );
+      throw new BadRequestException({
+        message: ExceptionMassage.PASSWROD_CONFIRM,
+        at: 'UserService.validateNewUser',
+      });
     }
-
-    const createdUser = new this.userRepository(signUpReqDto);
-    return createdUser.save();
   }
 
+  /**
+   * Find one user by email.
+   * @param email - The email to search for.
+   * @returns A promise that resolves to the User object.
+   */
   async findOneByEmail(email: string): Promise<User> {
-    return this.userRepository.findOne({ email: email });
+    return await this.userRepository.findOne({ email: email });
+  }
+
+  /**
+   * Hash the given password.
+   * @param password - The password to hash.
+   * @returns A promise that resolves to the hashed password.
+   */
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
   }
 }
