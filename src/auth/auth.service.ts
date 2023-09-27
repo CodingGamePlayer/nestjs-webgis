81686ea05fc2750a846cafb8dc6838583c27b6cc
KJS <otw1917@naver.com>
2023-09-27 21:26:55 +0900

    유저 로그인 방식 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,11 @@
-import {
-  Injectable,
-  UnauthorizedException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { SignInReqDto } from 'src/user/dto/req.dto';
 import { UserService } from 'src/user/user.service';
 import { SignInResDto } from './dto/res.dto';
 import { ExceptionMassage } from 'src/enums/excepion/exception';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/schema/user/user';
 
 @Injectable()
 export class AuthService {
@@ -16,9 +14,43 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  /**
+   * Authenticate a user and return an access token.
+   * @param signInReqDto - The user's sign-in credentials.
+   * @returns A promise that resolves to an access token.
+   */
   async signIn(signInReqDto: SignInReqDto): Promise<SignInResDto> {
-    const user = await this.userService.findOneByEmail(signInReqDto.email);
+    const validUser = await this.validateUser(
+      signInReqDto.email,
+      signInReqDto.password,
+    );
 
+    const { access_token } = await this.createToken(validUser);
+    return new SignInResDto(access_token);
+  }
+
+  /**
+   * Create an access token for the given user.
+   * @param user - The user for whom to create an access token.
+   * @returns A promise that resolves to an object containing the access token.
+   */
+  async createToken(user: User): Promise<{ access_token: string }> {
+    const payload = { email: user.email, sub: user._id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  /**
+   * Validate a user's credentials.
+   * @param email - The email to validate.
+   * @param pass - The password to validate.
+   * @returns A promise that resolves to the validated User object.
+   * @throws {BadRequestException} If the user is not found or password does not match.
+   */
+  async validateUser(email: string, pass: string): Promise<User> {
+    const user = await this.userService.findOneByEmail(email);
     if (!user) {
       throw new BadRequestException({
         message: ExceptionMassage.USER_NOT_FOUND,
@@ -26,20 +58,15 @@ export class AuthService {
       });
     }
 
-    if (user.password !== signInReqDto.password) {
-      throw new UnauthorizedException({
-        message: ExceptionMassage.INVALID_PASSWORD,
-        at: 'AuthService.signIn',
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (!isMatch) {
+      throw new BadRequestException({
+        message: ExceptionMassage.PASSWORD_NOT_MATCH,
+        at: 'AuthService.validateUser',
       });
     }
-    const { access_token } = await this.createToken(user);
-    return new SignInResDto(access_token);
-  }
 
-  async createToken(user: object) {
-    const payload = { email: user['email'], sub: user['_id'] };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    return user;
   }
 }
--- src/enums/excepion/exception.ts
+++ src/enums/excepion/exception.ts
@@ -3,4 +3,5 @@ export enum ExceptionMassage {
   USER_ALREADY_EXISTS = 'User already exists',
   PASSWROD_CONFIRM = 'Password and password confirmation must be the same',
   INVALID_PASSWORD = 'Password is not valid.',
+  PASSWORD_NOT_MATCH = 'Password is not match.',
 }
