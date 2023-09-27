5579f261eb42c71e0e00fbe9ee2179d2621c9f7b
김종식 <kim.jongsik@lumir-inc.com>
2023-09-27 12:53:05 +0900

    exception filter 에 로깅하는 기능 추가

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -3,16 +3,30 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const errorMessage = exception.message;
+
+    if (status < 500) {
+      this.logger.log(
+        `Status: ${status}, Error Message: ${errorMessage}, Method: ${request.method}, URL: ${request.url}`,
+      );
+    } else {
+      this.logger.error(
+        `Status: ${status}, Error Message: ${errorMessage}, Method: ${request.method}, URL: ${request.url}`,
+      );
+    }
 
     response.status(status).json({
       statusCode: status,
