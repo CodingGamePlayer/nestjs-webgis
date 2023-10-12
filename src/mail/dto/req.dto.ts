7a0c56ef7e67dfc4549bb504bb11dca3e311bede
김종식 <kim.jongsik@lumir-inc.com>
2023-10-12 18:04:19 +0900

    Docs: confirmation-emai
    - Swagger 추가
    - req.dto ApiProp 정보 추가

--- src/mail/dto/req.dto.ts
+++ src/mail/dto/req.dto.ts
@@ -1,16 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SendMailReqDto {
   @IsNotEmpty()
   @IsString()
   @IsEmail()
+  @ApiProperty({
+    type: String,
+    example: 'example@email.com',
+  })
   email: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    type: String,
+    example: 'John Doe',
+  })
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    type: String,
+    example: 'http://localhost:3000/auth/v1/confirm-email?token=123456789',
+  })
   confirmationLink: string;
 }
--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -2,14 +2,18 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { Public } from 'src/decorators/public-api.decoratpr';
 import { SendMailReqDto } from './dto/req.dto';
 import { MailService } from './mail.service';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiCommonResponses } from 'src/decorators/api-common-res.decorator';
 
 @Controller('mail/v1')
+@ApiTags('Mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Post('send-confirmation-email')
   @Public()
   @HttpCode(HttpStatus.OK)
+  @ApiCommonResponses()
   async sendConfirmationEmail(@Body() sendMailReqDto: SendMailReqDto) {
     await this.mailService.sendConfirmationEmail(sendMailReqDto);
   }
